620fa73d4a2f94dd4f9e8254661bf601d954c3ae
danielsagher <daniel.sagher1@gmail.com>
2024-06-04 22:15:49 -0400

    added state_scrape.py and comment code

--- python_files/main.py
+++ python_files/main.py
@@ -19,19 +19,24 @@ state_series = pd.read_csv(PATH + 'outputs/cleaning_op/state_series_dimension_cl
 def main(series_input, name_of_file):
 
     '''
-    This function takes in a list of seriesID's and a string which will 
-    used to name the CSV file output. Input can be any number of seriesID's 
-    greater than 1. 
+    This function takes in a list of seriesID's and a string 
+    used to name the CSV file output. Input can be any # of seriesID's > than 1. 
     '''
-    
-    data_results = derated_call(series_input, '2002', '2021')
+    # Takes in input amd year in str
+    # Defaults to 2001-2021 
+    # List len has to be >1
+    data_results = derated_call(series_input, '2002', '2015')
 
+
+    # Pulls out no-data messages and stores in DF
     message_list = message_retriever(data_results)
 
+    # Results of api caller run into a DF
     df = dataframe_maker(data_results)
     
-    df.to_csv(PATH + f'outputs/main_op/{name_of_file}_{NOW}.csv', index=False)
-    message_list.to_csv(PATH + f'outputs/main_op/{name_of_file}_msglst_{NOW}.csv', index=False)
+    # Outputs files into csv's w/ date and time
+    df.to_csv(f'{PATH}outputs/main_op/{name_of_file}_{NOW}.csv', index=False)
+    message_list.to_csv(f'{PATH}outputs/main_op/{name_of_file}_msglst_{NOW}.csv', index=False)
 
     
     time.sleep(1)
@@ -39,7 +44,7 @@ def main(series_input, name_of_file):
     
     return df
 
-
+# Run once for national_series, once for state_series
 final_national_df  = main(national_series, 'national_results')
 final_state_df = main(state_series, 'state_results')
 
